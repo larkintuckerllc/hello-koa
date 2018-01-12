d1ca60b62a8157a6befd679ed6a9f2e2e88fbba9
John  Tucker <john@larkintuckerllc.com>
2018-01-12 11:29:27 -0500

    express middleware

---
+++ express-middleware/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const express = require('express');
+const onHeaders = require('on-headers');
+
+const myAsync = () => new Promise(resolve => setTimeout(resolve, 1000, 'Hello World!'));
+const myMiddleware = (req, res, next) => {
+  const start = Date.now();
+  onHeaders(res, () => {
+    const end = Date.now();
+    res.set('X-Response-Time', `${(end - start).toString()} ms`);
+  });
+  // res.set('X-Response-Time', '0 ms'); // NOT FUNCTIONAL
+  next();
+};
+const app = express();
+app.use(myMiddleware);
+app.get('/', async (req, res) => {
+  const body = [];
+  body.push(await myAsync());
+  body.push(await myAsync());
+  res.send(body.join(' '));
+});
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
